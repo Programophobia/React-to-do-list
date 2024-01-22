92a71e2cca1d1f7fb82f9cc9cdb9b35e6c42f87b
Anna Powaska <annapowaska@op.pl>
2024-01-22 11:07:34 +0100

    Add icon choice to column

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,16 +2,20 @@ import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 
 const ColumnForm = props => {
-    const [value, setValue] = useState('');
-    const addColumn = e => {
+    const [title, setValue] = useState('');
+    const [icon, setIcon] = useState('')
+
+    const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: value });
+        props.action({ title: title, icon: icon });
+        setIcon('');
         setValue('');
     }
 
 	return (
-        <form onSubmit={addColumn}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+        <form onSubmit={handleSubmit}>
+           Title: <input type="text" value={title} onChange={e => setValue(e.target.value)} />
+           Icon: <input type="text"value={icon} onChange={e => setIcon(e.target.value)} />
             <button>Add column</button>
         </form>
 	);

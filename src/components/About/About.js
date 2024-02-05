d480c84a6307877a1112053ec8150d6c1a704c41
Anna Powaska <annapowaska@op.pl>
2024-02-05 11:02:29 +0100

    Add About and Favorite subpage

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import styles from './About.module.scss'
+import PageTitle from '../PageTitle/PageTitle'
+
+const About = () => {
+    return (
+        <section>
+            <container>
+                <PageTitle>about</PageTitle>
+                <div>lalala</div>
+            </container>
+        </section>
+    )  
+}
+
+export default About

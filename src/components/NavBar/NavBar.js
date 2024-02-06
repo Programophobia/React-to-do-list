6ae22cde49ec762fb2aa75d61504f4c406333372
Anna Powaska <annapowaska@op.pl>
2024-02-06 08:53:19 +0100

    Add preventDefault sensitive links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
-import styles from './NavBar.module.scss'
+import styles from './NavBar.module.scss';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav className={styles.navigation}>
             <a href="/"><i className="fa fa-tasks"></i></a>
             <ul className={styles.menu}>
-                <li><a href="/">HOME</a></li>
-                <li><a href="/favorite">FAVORITE</a></li>
-                <li><a href="/about">ABOUT</a></li>
+                <li><NavLink className={({isActive }) => isActive ? styles.linkActive : undefined} to="/">HOME</NavLink></li>
+                <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to="/favorite">FAVORITE</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">ABOUT</NavLink></li>
             </ul>
         </nav>
     )

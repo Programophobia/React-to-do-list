f04aa3ed6cf332831bc0bee1832cbc366f008b5d
Anna Powaska <annapowaska@op.pl>
2024-02-09 09:14:42 +0100

    cardsReducer modified

--- src/redux/cardsReducer.js
+++ src/redux/cardsReducer.js
@@ -1,4 +1,17 @@
 import shortid from "shortid";
+import strContains from '../utils/strContains'
+
+export const addCard = payload => ({ type: 'ADD_CARD', payload});
+export const getFilteredCards = ({ cards, searchString }, columnId) => cards
+ .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+ export const toggleCardFavorite = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
+export const getFavoriteCards = (state) => state.cards.filter(card => card.isFavorite);
+
+const createActionName = actionName => `app/cards/${actionName}`;
+const ADD_LIST = createActionName('ADD_CARD');
+
+export const addList = payload => ({ type: ADD_LIST, payload });
+
 
 const cardsReducer = (statePart = [], action) => {
     switch(action.type) {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,15 @@
 import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
-import strContains from '../utils/strContains'
 import listsReducer from './listsRedux';
 import columnsReducer from './columnsReducer';
 import cardsReducer from './cardsReducer';
 import searchStringReducer from './searchStringReducer';
 
-export const getFilteredCards = ({ cards, searchString }, columnId) => cards
- .filter(card => card.columnId === columnId && strContains(card.title, searchString));
 export const getAllColumns = state => state.columns;
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
-export const addCard = payload => ({ type: 'ADD_CARD', payload});
 export const updateSearchstring = payload => ({ type: 'UPDATE_SEARCHSTRING', payload});
 export const getColumnsByList = ({columns}, listId) => columns.filter(column => column.listId === listId);
 export const getSearchString = (state) => state.searchString;
-export const toggleCardFavorite = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
-export const getFavoriteCards = (state) => state.cards.filter(card => card.isFavorite);
 
 const subreducers = {
   lists: listsReducer,
